539d53994089d89f1188104c48d3f14304719a27
claudiom65 <claudio_marinelli@hotmail.com>
2016-06-26 11:28:37 +0000

    firsttttt

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello Codeee and Azure Web Apps! claudio first committtttt</h1>');
+    res.end('<h1>FIRST COMMIT!!!</h1>');
 }).listen(process.env.PORT);
